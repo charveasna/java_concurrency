e9ea94d72cf1959f82dde99f22176e4bf61e5aeb
Sovann Somaly <vchar@Sovanns-MacBook-Pro.local>
2017-02-28 23:08:44 +1100

    finalised ForkBlur

--- src/ForkBlur.java
+++ src/ForkBlur.java
@@ -1,11 +1,20 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.nio.Buffer;
 import java.security.spec.DSAGenParameterSpec;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
+
+import javax.imageio.ImageIO;
+import javax.swing.plaf.BorderUIResource.BevelBorderUIResource;
 
 public class ForkBlur extends RecursiveAction {
 	private int[] mSource;
 	private int mStart;
 	private int mLength;
 	private int[] mDestination;
+	
+	protected static int sThreshold = 100000;
 	
 	// Processing window size; should be odd.
 	private int mBlurWidth = 15;
@@ -35,5 +44,60 @@ public class ForkBlur extends RecursiveAction {
 					| (((int)bt) << 0);
 			mDestination[index] = dpixel;
 		}
+	}
+	
+	protected void compute() {
+		if (mLength < sThreshold) {
+			computeDirectly();
+			return;
+		}
+		
+		int split = mLength / 2;
+		invokeAll(new ForkBlur(mSource, mStart, split, mDestination),
+				new ForkBlur(mSource, mStart + split, mLength - split, mDestination));
+	}
+	
+	// Plumbing follows.
+	public static void main(String[] args) throws Exception {
+		String srcName = "angkor.jpg";
+		File srcFile = new File(srcName);
+		BufferedImage image = ImageIO.read(srcFile);
+		
+		System.out.println("Source image: " + srcName);
+		
+		BufferedImage blurredImage = blur(image);
+		
+		String dstName = "blurred-angkor.jpg";
+		File dstFile = new File(dstName);
+		ImageIO.write(blurredImage, "jpg", dstFile);
+		
+		System.out.println("Output image: " + dstName);
+	}
+	
+	public static BufferedImage blur(BufferedImage srcImage) {
+		int w = srcImage.getWidth();
+		int h = srcImage.getHeight();
+		
+		int[] src = srcImage.getRGB(0, 0, w, h, null, 0, w);
+		int[] dst = new int[src.length];
+		
+		System.out.println("Array size is " + src.length);
+		System.out.println("Threshold is " + sThreshold);
+		
+		int processors = Runtime.getRuntime().availableProcessors();
+		System.out.println(Integer.toString(processors) + " processor"
+				+ (processors != 1 ? "s are " : " is " ) + " available.");
+		
+		ForkBlur fb = new ForkBlur(src, 0, src.length, dst);
+		ForkJoinPool pool = new ForkJoinPool();
+		long startTime = System.currentTimeMillis();
+		pool.invoke(fb);
+		long endTime = System.currentTimeMillis();
+		
+		System.out.println("Image blur took " + (endTime-startTime) + " milliseconds.");
+		BufferedImage dstImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+		dstImage.setRGB(0, 0, w, h, dst, 0, w);
+		
+		return dstImage;
 	}
 }
